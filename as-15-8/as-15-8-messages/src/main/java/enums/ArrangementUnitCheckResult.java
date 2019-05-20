87b8fc3b2dde247d21b711a81c68e3d08e41d4cc
ashabalin <ashabalin@ec-leasing.ru>
2019-05-20 17:14:56 +0300

    Список возможных результатов проверки единиц мероприятия

---
+++ as-15-8/as-15-8-messages/src/main/java/enums/ArrangementUnitCheckResult.java
@@ -0,0 +1,21 @@
+package enums;
+
+/**
+ * Результат проверки единицы мероприятия
+ * @author shabalinAI
+ *
+ */
+public enum ArrangementUnitCheckResult {
+
+	COMPLETED,
+	FORBIDDEN_CONTENT_DETECTED,
+	CAPTCHA_DETECTED,
+	
+	DNS_ERROR,
+	SOCKET_ERROR,
+	HTTP_SERVER_SEND_NO_RESPONSE,
+	PAGE_NOT_FOUND,
+	
+	INTERNAL_ERROR,
+	TIMEOUT_ERROR;
+}
