b4772b425922fd92372b39ce85557270061b3d53
asavin <asavin@ec-leasing.ru>
2019-06-14 09:18:11 +0300

    Добавил профили прода

--- as-15-8/as-15-8-rest/src/main/java/controllers/GlobalParametersController.java
+++ as-15-8/as-15-8-rest/src/main/java/controllers/GlobalParametersController.java
@@ -7,7 +7,6 @@ import model.parameters.GlobalParameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
