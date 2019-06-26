01d5e6b5970918c17670e36a920df6f169ad5391
pvorobiev <vorobievp>
2019-06-26 14:00:58 +0300

    лил

--- as-15-8/as-15-8-modules/as-15-8-analyzer/src/main/java/analysis/AnalysisUtils.java
+++ as-15-8/as-15-8-modules/as-15-8-analyzer/src/main/java/analysis/AnalysisUtils.java
@@ -253,7 +253,7 @@ public class AnalysisUtils {
 
         if (!isEmpty(errorCode)){
             if (errorCode.contains(INTERNAL_ERROR.name())) {
-                message = "Ошибка! " + errorCode;
+                message = "Ошибка ЭТАЛОНА! " + errorCode;
                 result = INTERNAL_ERROR;
             }
             else if (errorCode.contains("NO_INTERNET")) {
