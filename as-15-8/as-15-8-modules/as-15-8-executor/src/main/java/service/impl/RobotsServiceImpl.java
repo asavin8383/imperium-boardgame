a7ad5668ce04eb2e356dc2dbb1a34555742519bf
ashabalin <ashabalin@ec-leasing.ru>
2019-06-27 14:21:30 +0300

    Добавил фазу

--- as-15-8/as-15-8-modules/as-15-8-executor/src/main/java/service/impl/RobotsServiceImpl.java
+++ as-15-8/as-15-8-modules/as-15-8-executor/src/main/java/service/impl/RobotsServiceImpl.java
@@ -11,6 +11,7 @@ import java.util.concurrent.ExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.AbstractMessageListenerContainer;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.messaging.Message;
@@ -237,5 +238,10 @@ public class RobotsServiceImpl implements RobotsService {
 	@Override
 	public boolean isRunning() {
 		return this.isRunning;
+	}
+	
+	@Override
+	public int getPhase() {
+		return AbstractMessageListenerContainer.DEFAULT_PHASE + 10;
 	}
 }
