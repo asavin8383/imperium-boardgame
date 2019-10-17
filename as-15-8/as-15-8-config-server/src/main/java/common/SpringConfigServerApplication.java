103fe52546d9a29361ba4a9747bc8d675504c49a
ashabalin <ashabalin@ec-leasing.ru>
2019-10-17 12:05:56 +0300

    config server fix

--- as-15-8/as-15-8-config-server/src/main/java/common/SpringConfigServerApplication.java
+++ as-15-8/as-15-8-config-server/src/main/java/common/SpringConfigServerApplication.java
@@ -3,7 +3,6 @@ package common;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.config.monitor.PropertyPathEndpoint;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.ComponentScan;
 
