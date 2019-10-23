da01df27bc9fee79a79d0eda37591705ee2d6638
asavin <asavin8383@gmail.com>
2019-10-23 11:29:05 +0300

    ПОД привёл всё к единому виду

--- as-15-8/as-15-8-uploader/src/main/java/controllers/PsController.java
+++ as-15-8/as-15-8-uploader/src/main/java/controllers/PsController.java
@@ -47,7 +47,7 @@ public class PsController {
                     Utils.getEndDate(), query, page);
             return new ResponseEntity<>(result, HttpStatus.OK);
         } else {
-            return new ResponseEntity<>(null, HttpStatus.ACCEPTED);
+            return new ResponseEntity<>(null, HttpStatus.OK);
         }
     }
 
