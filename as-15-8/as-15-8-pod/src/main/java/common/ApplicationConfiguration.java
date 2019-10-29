e81c3df93d3d2dd00dd5f8ec71e41b1afd872209
asavin <asavin8383@gmail.com>
2019-10-29 11:36:36 +0300

    POD убрал security

--- as-15-8/as-15-8-pod/src/main/java/common/ApplicationConfiguration.java
+++ as-15-8/as-15-8-pod/src/main/java/common/ApplicationConfiguration.java
@@ -1,34 +1,18 @@
 package common;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.security.Principal;
-import java.util.Arrays;
 
 
 /**
@@ -42,12 +26,9 @@ import java.util.Arrays;
 @EntityScan("model")
 @EnableScheduling
 @EnableAsync
-@EnableResourceServer
+//@EnableResourceServer
 @RestController
-public class ApplicationConfiguration extends ResourceServerConfigurerAdapter {
-
-	@Value("${jwt.key}")
-	private String jwtKey;
+public class ApplicationConfiguration {
 
 	// todo async security
 	/*@PostConstruct
@@ -62,15 +43,6 @@ public class ApplicationConfiguration extends ResourceServerConfigurerAdapter {
 
     public static void main(String[] args) {
 		SpringApplication.run(ApplicationConfiguration.class, args);
-	}
-
-	@Override
-	public void configure(final HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-				.antMatchers("/oauth/**")
-				.permitAll()
-				.antMatchers("/**")
-				.authenticated();
 	}
 
 	@PostConstruct
