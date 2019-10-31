b7b01b3ffe3b818e9c41db34aa660ab2e48eddb1
pvorobiev <vorobievp>
2019-10-31 14:42:17 +0300

    fix

--- as-15-8/as-15-8-pmk/as-15-8-analyzer/src/main/java/common/ApplicationConfiguration.java
+++ as-15-8/as-15-8-pmk/as-15-8-analyzer/src/main/java/common/ApplicationConfiguration.java
@@ -3,6 +3,7 @@ package common;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
 /**
  * Конфигурация модуля запуска проверок мероприятий
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.ComponentScan;
  *
  */
 @SpringBootApplication
+@Import(RestTemplateConfiguration.class)
 @ComponentScan(basePackages={"common", "events", "service", "restapi"})
 public class ApplicationConfiguration{
 	
