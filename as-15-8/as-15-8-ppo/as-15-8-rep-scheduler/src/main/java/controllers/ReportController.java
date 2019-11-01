9c8fbb19e388ce9a06f03073dfd2f86465a26fe4
asin <asinjavin@ec-leasing.ru>
2019-11-01 15:50:18 +0300

    1. Запрос на получение статистики по сформированным регламентным отчетам select * from dm.v_api_reg_reports_stat; 2. Запрос на получение описания отчета по типу отчета select * from dm.rep_tp where rep_tp_id = ? 3. Запрос на получение таблицы по типу отчета select * from dm.v_api_reg_reports_table where rep_tp_id=? 4. Запрос получения файла  отчета по rep_id и format

--- as-15-8/as-15-8-ppo/as-15-8-rep-scheduler/src/main/java/controllers/ReportController.java
+++ as-15-8/as-15-8-ppo/as-15-8-rep-scheduler/src/main/java/controllers/ReportController.java
@@ -1,30 +1,112 @@
 package controllers;
 
-import model.Report;
+import controllers.ex.ReportNotCreated;
+import controllers.ex.ReportNotFound;
+import model.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import repositories.RegReportsTableRepository;
 import repositories.ReportRepository;
+import repositories.ReportStatRepository;
+import repositories.ReportTypeRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * User: asinjavin
  * Date: 31.10.2019
  * Time: 19:21
  */
-@RestController("reports")
+@RestController
+@RequestMapping("reports")
 public class ReportController
 {
+    private final ReportStatRepository reportStatRepository;
+    private final RegReportsTableRepository regReportsTableRepository;
     private final ReportRepository reportRepository;
+    private final ReportTypeRepository reportTypeRepository;
 
     @Autowired
-    public ReportController(ReportRepository reportRepository) {
+    public ReportController(ReportStatRepository reportStatRepository,
+                            RegReportsTableRepository regReportsTableRepository,
+                            ReportRepository reportRepository, ReportTypeRepository reportTypeRepository) {
+        this.reportStatRepository = reportStatRepository;
+        this.regReportsTableRepository = regReportsTableRepository;
         this.reportRepository = reportRepository;
+        this.reportTypeRepository = reportTypeRepository;
     }
 
-    @GetMapping()
-    public List<Report> reportList() {
-        return reportRepository.findAll();
+    /**
+     * 1 Запрос на получение статистики по сформированным регламентным отчетам
+     * select * from dm.v_api_reg_reports_stat;
+     *
+     * @return
+     */
+    @GetMapping("stat")
+    public List<ReportStat> getStat() {
+        return reportStatRepository.findAll();
     }
+
+    /**
+     * 2 Запрос на получение описания отчета по типу отчета
+     * select * from dm.rep_tp where rep_tp_id = ?
+     *
+     * @param rep_tp_id
+     * @return
+     */
+    @GetMapping("{rep_tp_id}")
+    public ReportType getReportType(@PathVariable long rep_tp_id) {
+        Optional<ReportType> res = reportTypeRepository.findById(rep_tp_id);
+        if (!res.isPresent()) throw new ReportNotFound();
+        return res.get();
+    }
+
+    /**
+     * 3 Запрос на получение таблицы по типу отчета
+     * select * from dm.v_api_reg_reports_table where rep_tp_id=?
+     *
+     * @return
+     */
+    @GetMapping("table/{rep_tp_id}")
+    public List<RegReportsTable> getRegReportsTable(@PathVariable long rep_tp_id) {
+        return regReportsTableRepository.findAllByRepTpId(rep_tp_id);
+    }
+
+
+    /**
+     * 4. Запрос получения файла отчета по rep_id
+     * @param rep_id
+     * @return
+     */
+    @GetMapping("data/{rep_id}")
+    ResponseEntity<byte[]> getData(@PathVariable long rep_id) {
+        Optional<Report> res = reportRepository.findByRepId(rep_id);
+        if (!res.isPresent()) throw new ReportNotFound();
+
+        Report report = res.get();
+        if (report.getStatus() != ReportStatus.OK) throw new ReportNotCreated();
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.setContentType(MediaType.parseMediaType(report.getMime()));
+
+        return new ResponseEntity<byte[]>(res.get().getData(), responseHeaders, HttpStatus.OK);
+    }
+
+    /*
+5. Запрос для получения статистики о отчетах для админов
+select * from dm.v_api_reg_reports_admin_stat;
+6. Запрос для таблицы для админа по типу отчета
+select * from  v_api_reg_reports_admin_table rep_tp_id=?
+7. Запрос по перевыпуску отчета по rep_id и format
+     */
+
+
 }
--- as-15-8/as-15-8-ppo/as-15-8-rep-scheduler/src/main/java/repositories/ReportRepository.java
+++ as-15-8/as-15-8-ppo/as-15-8-rep-scheduler/src/main/java/repositories/ReportRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * User: asinjavin
@@ -15,4 +16,8 @@ public interface ReportRepository extends JpaRepository<Report, Long>
 {
     @Query(value = "SELECT r FROM Report r WHERE msr_prd_end_dttm < current_timestamp and status = 'NEW'")
     List<Report> getTodo();
+
+    Optional<Report> findByRepIdAndFormat(long rep_id,String format);
+
+    Optional<Report> findByRepId(long rep_id);
 }
