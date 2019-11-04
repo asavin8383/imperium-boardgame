ae792383d6c5025dc3c0588385dfbd2009381ff8
asavin <asavin8383@gmail.com>
2019-11-04 16:32:24 +0300

    dispatcher исправил логи

--- as-15-8/as-15-8-ppm/src/main/java/repositories/ScheduleRepo.java
+++ as-15-8/as-15-8-ppm/src/main/java/repositories/ScheduleRepo.java
@@ -3,6 +3,11 @@ package repositories;
 import model.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface ScheduleRepo extends JpaRepository<Schedule, Long> {
+
+    List<Schedule> findAllByPlannedDate(LocalDate plannedDate);
 
 }
