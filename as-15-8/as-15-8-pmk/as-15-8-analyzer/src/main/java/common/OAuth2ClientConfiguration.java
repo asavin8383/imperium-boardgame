2a4cf47aa0072fd42fe50774834630ee8f639082
ashabalin <ashabalin@ec-leasing.ru>
2019-11-04 11:37:53 +0300

    eureka and config server with oauth2

--- as-15-8/as-15-8-pmk/as-15-8-analyzer/src/main/java/common/OAuth2ClientConfiguration.java
+++ as-15-8/as-15-8-pmk/as-15-8-analyzer/src/main/java/common/OAuth2ClientConfiguration.java
@@ -7,7 +7,6 @@ import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
-import org.springframework.web.client.RestTemplate;
 
 
 @Configuration
@@ -23,6 +22,7 @@ public class OAuth2ClientConfiguration {
                                                  OAuth2ProtectedResourceDetails details) {
         return new OAuth2RestTemplate(details, oauth2ClientContext);
     }
+
 
 
 }
---
+++ as-15-8/as-15-8-smuf/as-15-8-discovery/src/main/java/common/ResourceServerConfiguration.java
@@ -0,0 +1,29 @@
+package common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+            .antMatcher("/**")
+                .authorizeRequests()
+                    .antMatchers(HttpMethod.GET,
+                            "/",
+                            "/lastn",
+                            "/eureka/js/**",
+                            "/eureka/css/**",
+                            "/eureka/images/**",
+                            "/eureka/fonts/**").permitAll()
+                    .antMatchers("/info", "/health").permitAll()
+                .anyRequest().hasRole("SYSTEM")
+                .and().httpBasic().and().csrf().disable();
+    }
+}
