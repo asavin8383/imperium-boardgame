eb42e9db8c9a2ea733ac18d8d89b164f21df577b
ashabalin <ashabalin@ec-leasing.ru>
2019-11-05 10:39:58 +0300

    role to token

--- as-15-8/as-15-8-soib/as-15-8-auth-server/src/main/java/common/CustomTokenEnhancer.java
+++ as-15-8/as-15-8-soib/as-15-8-auth-server/src/main/java/common/CustomTokenEnhancer.java
@@ -7,12 +7,17 @@ import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class CustomTokenEnhancer implements TokenEnhancer {
     @Override
     public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
         final Map<String, Object> additionalInfo = new HashMap<>();
         additionalInfo.put("organization", "ecl");
+        additionalInfo.put("role", authentication.getAuthorities()
+                .stream()
+                .map(at -> at.getAuthority())
+                .collect(Collectors.joining(",")));
         ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInfo);
         return accessToken;
     }
