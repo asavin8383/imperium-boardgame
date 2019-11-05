4c1cb41a43d0f0b38d6b9b4ca51a00c33a29e707
ashabalin <ashabalin@ec-leasing.ru>
2019-11-05 13:47:37 +0300

    cors test

--- as-15-8/as-15-8-smuf/as-15-8-gateway/src/main/java/common/SpringGatewayApplication.java
+++ as-15-8/as-15-8-smuf/as-15-8-gateway/src/main/java/common/SpringGatewayApplication.java
@@ -2,14 +2,7 @@ package common;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -17,20 +10,6 @@ public class SpringGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringGatewayApplication.class, args);
-    }
-
-    @Bean
-    public FilterRegistrationBean<CorsFilter> corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
-        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
-        bean.setOrder(0);
-        return bean;
     }
 
 }
