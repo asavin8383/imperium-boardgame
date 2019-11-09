64a969637b5a43bff4ece7259ff4c5134f37e387
asavin <asavin8383@gmail.com>
2019-11-09 16:06:32 +0300

    ППM Получение фильтрованного списка чек-юнитов по ИД мероприятия

--- as-15-8/as-15-8-ppm/src/main/java/repositories/ScheduleCheckUnitRepo.java
+++ as-15-8/as-15-8-ppm/src/main/java/repositories/ScheduleCheckUnitRepo.java
@@ -2,7 +2,12 @@ package repositories;
 
 import model.Arrangement;
 import model.ScheduleCheckUnit;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +16,12 @@ import java.util.List;
 public interface ScheduleCheckUnitRepo extends JpaRepository<ScheduleCheckUnit, Long> {
 
     List<ScheduleCheckUnit> findAllByArrangement(Arrangement arrangement);
+
+    Page<ScheduleCheckUnit> findAllByArrangement(Arrangement arrangement, Pageable pageable);
+
+    @Query("select DISTINCT scu from ScheduleCheckUnit scu" +
+            " where scu.arrangement = :ar and" +
+            " (scu.checkUnitValue LIKE CONCAT('%',:query,'%') or scu.checkUnitType LIKE CONCAT('%',:query,'%') or scu.erdiId LIKE CONCAT('%',:query,'%'))")
+    Page<ScheduleCheckUnit> findAllByArrangement(@Param("ar") Arrangement arrangement, @Param("query")String query, Pageable pageable);
+
 }
