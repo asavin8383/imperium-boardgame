e284d4bb3d92736a5bb455f44fa6c007dda2dfd3
ashabalin <ashabalin@ec-leasing.ru>
2019-11-19 15:33:30 +0300

    error for screenshot script

--- as-15-8/as-15-8-pmk/as-15-8-executor/src/main/resources/takeScreenshot.js
+++ as-15-8/as-15-8-pmk/as-15-8-executor/src/main/resources/takeScreenshot.js
@@ -1,27 +1,30 @@
 let done = arguments[0];
-try {
-    const displayMediaOptions = {
-        video: {
-            cursor: "never"
-        },
-        audio: false
-    };
-    navigator.mediaDevices.getDisplayMedia(displayMediaOptions)
-        .then(stream => {
-            setTimeout(function () {
-                let track = stream.getVideoTracks()[0];
-                let imgCapture = new ImageCapture(track);
-                imgCapture.grabFrame()
-                .then(bitmap => {
-                    let canvas = document.createElement('canvas');
-                    canvas.width = bitmap.width;
-                    canvas.height = bitmap.height;
-                    let context = canvas.getContext('2d');
-                    context.drawImage(bitmap, 0, 0);
-                    done(canvas.toDataURL('image/png;base64,').replace('data:image/png;base64,', ''));
-                });
-            }, 3000);
+const displayMediaOptions = {
+    video: {
+        cursor: "never"
+    },
+    audio: false
+};
+navigator.mediaDevices.getDisplayMedia(displayMediaOptions)
+    .then(stream => {
+        setTimeout(function () {
+            let track = stream.getVideoTracks()[0];
+            let imgCapture = new ImageCapture(track);
+            imgCapture.grabFrame()
+            .then(bitmap => {
+                let canvas = document.createElement('canvas');
+                canvas.width = bitmap.width;
+                canvas.height = bitmap.height;
+                let context = canvas.getContext('2d');
+                context.drawImage(bitmap, 0, 0);
+                done(canvas.toDataURL('image/png;base64,').replace('data:image/png;base64,', ''));
+            }).catch(ex => {
+                done("Error: " + ex.toString())
+            });
+        }, 3000)
+        .catch(ex => {
+            done("Error: " + ex.toString())
         });
-} catch(err) {
-    done("Error: " + err.toString());
-}
+    }).catch(ex => {
+         done("Error: " + ex.toString())
+    });
