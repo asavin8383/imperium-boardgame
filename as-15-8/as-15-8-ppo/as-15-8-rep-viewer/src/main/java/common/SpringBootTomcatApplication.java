e5f4bd300610bc467bd44ee33d5dbcc1382c18ad
asin <asinjavin@ec-leasing.ru>
2019-11-22 04:20:49 +0300

    It works + spring

--- as-15-8/as-15-8-ppo/as-15-8-rep-viewer/src/main/java/common/SpringBootTomcatApplication.java
+++ as-15-8/as-15-8-ppo/as-15-8-rep-viewer/src/main/java/common/SpringBootTomcatApplication.java
@@ -1,11 +1,17 @@
 package common;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 /**
  * User: asinjavin
  * Date: 21.11.2019
  * Time: 22:47
  */
-//@SpringBootApplication
-public class SpringBootTomcatApplication
+@SpringBootApplication
+//        (exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+public class SpringBootTomcatApplication extends SpringBootServletInitializer
 {
 }
