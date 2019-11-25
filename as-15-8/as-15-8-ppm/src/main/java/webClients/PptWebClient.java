64a6523dd8902e691fcf21a58d7b5c0e4b051f2b
ashabalin <ashabalin@ec-leasing.ru>
2019-11-25 17:45:47 +0300

    PPM add retrive check units timeout

--- as-15-8/as-15-8-ppm/src/main/java/webClients/PptWebClient.java
+++ as-15-8/as-15-8-ppm/src/main/java/webClients/PptWebClient.java
@@ -2,18 +2,24 @@ package webClients;
 
 import checkUnits.CheckUnit;
 import exceptions.AS_15_8_PPM_Exception;
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.client.reactive.ClientHttpConnector;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
+import reactor.netty.http.client.HttpClient;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 
 /**
@@ -27,6 +33,8 @@ public class PptWebClient {
 
     private final String CHECK_UNITS_URI = "/ppt/arrangements/checkUnits";
 
+    private final Long requestTimeout = 3600000L;
+
     @Value("${gateway.url}")
     private String gatewayUrl;
 
@@ -35,7 +43,19 @@ public class PptWebClient {
         try {
             log.info("Получение чек-юнитов мероприятия {} по запросу: {}", arrangementId, uri);
 
-            return WebClient.create(gatewayUrl)
+            HttpClient httpClient = HttpClient.create()
+                    .tcpConfiguration(tcpClient -> {
+                        tcpClient = tcpClient.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, requestTimeout.intValue());
+                        tcpClient = tcpClient.doOnConnected(conn -> conn
+                                .addHandlerLast(new ReadTimeoutHandler(requestTimeout, TimeUnit.MILLISECONDS)));
+                        return tcpClient;
+                    });
+
+            return WebClient
+                    .builder()
+                    .clientConnector(new ReactorClientHttpConnector(httpClient))
+                    .baseUrl(gatewayUrl)
+                    .build()
                     .get()
                     .uri(uri)
                     .retrieve()
