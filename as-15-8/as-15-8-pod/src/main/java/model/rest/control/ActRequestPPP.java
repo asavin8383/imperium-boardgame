45a5d86e6abc0ecff348a208473899a62d5f952b
pvorobiev <pvorobiev@ec-leasing.ru>
2019-11-25 12:59:58 +0300

    fix

--- as-15-8/as-15-8-pod/src/main/java/model/rest/control/ActRequestPPP.java
+++ as-15-8/as-15-8-pod/src/main/java/model/rest/control/ActRequestPPP.java
@@ -34,6 +34,6 @@ public class ActRequestPPP {
     private String endDate;     // YYYY-MM-DDTHH24:MI:SS
 
     @ToString.Exclude
-    @JsonProperty("CheckResultId")
+    @JsonProperty("CheckResults")
     private List<ActCheckResultPPP> checkResults;
 }
