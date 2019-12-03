147bf721bef20ab27b72e0ac8e305054f64791c5
Kirill Lychagin <klychagin@ec-leasing.ru>
2019-12-03 23:58:58 +0300

    работает взаимодействие с эврикой

--- as-15-8/as-15-8-ppo/as-15-8-rep-gate/init_eureka.sh
+++ as-15-8/as-15-8-ppo/as-15-8-rep-gate/init_eureka.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-token=`curl -s --request POST   --url $GATEWAY_URI'/security/oauth/token?grant_type=client_credentials&clientid='$CLIENTID'&client_secret='$CLIENT_SECRET   --header 'authorization: Basic '${BASIC_AUTH}  | jq -r .access_token`
+token=`curl -s --request POST   --url "$GATEWAY_URI"'/security/oauth/token?grant_type=client_credentials&clientid='"$CLIENTID"'&client_secret='"$CLIENT_SECRET"   --header 'authorization: Basic '"${BASIC_AUTH}"  | jq -r .access_token`
 curl -s --request POST \
   --url $GATEWAY_URI/eureka/apps/birt-viewer \
-  --header 'authorization: Bearer '${token} \
+  --header 'authorization: Bearer '"${token}" \
   --header 'content-type: application/json' \
-  --data '@/config/eureka_init_'$PROFILE'.json'
+  --data '@/config/eureka_init_'"$PROFILE"'.json'
