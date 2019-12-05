03ffafdb4f03315a7d93595ea10fcde707bb7eb8
pvorobiev <pvorobiev@ec-leasing.ru>
2019-12-05 14:11:31 +0300

    сырая версия редактирования роботов пс/пасд

--- as-15-8/as-15-8-smuf/as-15-8-config-server/src/main/java/common/SpringConfigServerApplication.java
+++ as-15-8/as-15-8-smuf/as-15-8-config-server/src/main/java/common/SpringConfigServerApplication.java
@@ -6,12 +6,14 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableConfigServer
-@ComponentScan(basePackages = {"common", "model", "controllers"})
+@ComponentScan(basePackages = {"common", "model", "controllers", "services", "repositories"})
 @EntityScan("model")
 @EnableJpaRepositories("repositories")
+@RestController
 public class SpringConfigServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringConfigServerApplication.class, args);
