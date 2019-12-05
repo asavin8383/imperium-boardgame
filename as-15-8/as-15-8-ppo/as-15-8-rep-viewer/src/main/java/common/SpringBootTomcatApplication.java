f5d59ba677ae156c245ea67e03f805a7ee272e05
asin <asinjavin@ec-leasing.ru>
2019-12-05 15:40:13 +0300

    Spring Boot initial

--- as-15-8/as-15-8-ppo/as-15-8-rep-viewer/src/main/java/common/SpringBootTomcatApplication.java
+++ as-15-8/as-15-8-ppo/as-15-8-rep-viewer/src/main/java/common/SpringBootTomcatApplication.java
@@ -1,13 +1,18 @@
 package common;
 
 import model.soap.SoapOperation;
+import org.eclipse.birt.report.servlet.BirtEngineServlet;
+import org.eclipse.birt.report.servlet.ViewerServlet;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.*;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.context.WebApplicationContext;
 
+import javax.servlet.Servlet;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,6 +28,10 @@ import java.util.Map;
 @Configuration
 public class SpringBootTomcatApplication extends SpringBootServletInitializer
 {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTomcatApplication.class, args);
+    }
+
     @Bean
     @Scope(value = WebApplicationContext.SCOPE_SESSION, proxyMode = ScopedProxyMode.TARGET_CLASS)
     public Map<String, SoapOperation> requests() {
