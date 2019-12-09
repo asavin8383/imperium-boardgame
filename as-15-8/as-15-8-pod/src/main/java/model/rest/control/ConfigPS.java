94d827824709f295ef240dd0783bc3407db02a45
ashabalin <ashabalin@ec-leasing.ru>
2019-12-09 14:52:16 +0300

    fix save ps/pasd

--- as-15-8/as-15-8-pod/src/main/java/model/rest/control/ConfigPS.java
+++ as-15-8/as-15-8-pod/src/main/java/model/rest/control/ConfigPS.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ConfigPS
 {
-    private Long id;
+    private Long origId;
     private String name;
     private String hostname;
 }
