9776a2f54181ae3e6585f1fc01ffd1aef75acbb8
klychagin <klycahign@ec-leasing.ru>
2019-12-10 20:53:42 +0300

    исправлен профайл

--- as-15-8/as-15-8-ppo/as-15-8-rep-gate/heartbeat_eureka.sh
+++ as-15-8/as-15-8-ppo/as-15-8-rep-gate/heartbeat_eureka.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 token=`curl  -s --request POST   --url $GATEWAY_URI'/security/oauth/token?grant_type=client_credentials&clientid='$CLIENTID'&client_secret='$CLIENT_SECRET   --header 'authorization: Basic '${BASIC_AUTH}  | jq -r .access_token`
-instanceId=`cat /config/eureka_init_test.json | jq -r .instance.instanceId`
+instanceId=`cat /config/eureka_init_"$PROFILE".json | jq -r .instance.instanceId`
 ./init_eureka.sh    
 echo "sending hearthbeat ..."
 curl -s -I --request PUT \
