56e2b627a01d654ecf12b60046d864759de65a9a
danikanov <danikanov@ec-leasing.ru>
2019-12-12 19:02:49 +0300

    Реализовал контроллер для пинга реестра

---
+++ as-15-8/as-15-8-pod/src/main/java/controllers/PppRaCheckController.java
@@ -0,0 +1,24 @@
+package controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import restapi.PPPRACheckClient;
+
+@RestController
+@RequestMapping(path = "/pppracheck")
+@PreAuthorize("hasAnyRole('ROLE_VIEW_RESULT')")
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+
+public class PppRaCheckController {
+
+    private final PPPRACheckClient ppraCheckClient;
+
+    @GetMapping
+    public boolean isRegistryAvailable() {
+        return ppraCheckClient.checkRegistryIsAvailable();
+    }
+}
