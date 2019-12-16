8479d5a878f9b0064a57be170edc084b40232411
danikanov <danikanov@ec-leasing.ru>
2019-12-16 13:49:34 +0300

    build exception fix

--- as-15-8/as-15-8-ppm/src/main/java/controllers/ArrangementController.java
+++ as-15-8/as-15-8-ppm/src/main/java/controllers/ArrangementController.java
@@ -99,10 +99,12 @@ public class ArrangementController {
     private void getAndSaveArrangementCheckUnits(Arrangement arrangement){
         arrangement.getScheduleCheckUnits().addAll(
             PPTWebClient.getCheckUnitsByArrangementId(arrangement.getId())
-                .stream()
-                .flatMap(checkUnit -> createCheckUnits(arrangement, checkUnit).stream())
+                .toStream()
+                .flatMap(checkUnits -> checkUnits.stream().flatMap(checkUnit -> createCheckUnits(arrangement, checkUnit).stream())
+                )
                 .collect(Collectors.toList())
         );
+        //toStream().flatMap(List::stream).collect(Collectors.toList());
         log.info("Получен список чек-юнитов мероприятия {} от ППТ", arrangement.getId());
         if(arrangement.getScheduleCheckUnits().size() > 0){
             arrangementRepo.save(arrangement);
