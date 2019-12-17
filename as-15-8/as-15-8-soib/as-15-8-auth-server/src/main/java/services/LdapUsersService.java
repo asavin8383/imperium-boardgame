7847760533a0d17ef66573453979e99b2e85cffb
danikanov <danikanov@ec-leasing.ru>
2019-12-17 17:48:50 +0300

    Auth server - переделал поиск юзеров. Теперь пользователи вытягиваются по роли ROLE_FORMAL_TASK ППТ - SearchTemplateController - сменил авторизацию на ROLE_PREPARATION_TRAFFIC Role enum - удалён

--- as-15-8/as-15-8-soib/as-15-8-auth-server/src/main/java/services/LdapUsersService.java
+++ as-15-8/as-15-8-soib/as-15-8-auth-server/src/main/java/services/LdapUsersService.java
@@ -10,28 +10,56 @@ import org.springframework.ldap.query.LdapQueryBuilder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class LdapUsersService {
 
     private final LdapTemplate ldapTemplate;
-
+    private String base;
 
 
     public List<String> getUserNamesByRole(String role){
-        final String base = ((LdapContextSource)ldapTemplate.getContextSource()).getBaseLdapPathAsString();
 
-        LdapQuery ldapQuery = LdapQueryBuilder
+        base = ((LdapContextSource)ldapTemplate.getContextSource()).getBaseLdapPathAsString();
+
+        return searchRoles(createRolesQuery(role)).stream()
+                .map(fRole -> searchUserNames(createUsersQuery(fRole)))
+                .flatMap(List::stream)
+                .collect(Collectors.toList());
+
+    }
+
+    private List<String> searchRoles(LdapQuery ldapQuery) {
+        return search("cn", ldapQuery);
+    }
+
+    private List<String> searchUserNames(LdapQuery ldapQuery) {
+        return search("displayname", ldapQuery);
+    }
+
+    private List<String> search(String param, LdapQuery ldapQuery) {
+        return ldapTemplate.search(ldapQuery,
+                (AttributesMapper<String>) attributes ->
+                        attributes.get(param) == null ? "" : attributes.get(param).get().toString()
+        );
+    }
+
+    private LdapQuery createUsersQuery(String role) {
+        return LdapQueryBuilder
                 .query()
                 .where("objectClass")
                 .is("user")
-                .and("memberOf").is("CN="+role+",OU=roles,"+base);
+                .and("memberOf").is("CN=" + role + ",OU=roles," + base);
+    }
 
-        return ldapTemplate.search(ldapQuery,
-                (AttributesMapper<String>) attributes ->
-                        attributes.get("displayname") == null ? "" : attributes.get("displayname").get().toString()
-        );
+    private LdapQuery  createRolesQuery(String role) {
+        return LdapQueryBuilder
+                .query()
+                .where("objectClass")
+                .is("group")
+                .and("memberOf").is("CN=" + role + ",OU=functional_roles,OU=roles," + base);
     }
 
 }
