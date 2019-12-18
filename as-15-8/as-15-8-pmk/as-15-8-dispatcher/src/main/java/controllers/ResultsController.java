918848ae8b7767e5cc31a9dd77067e45ea605ed3
danikanov <danikanov@ec-leasing.ru>
2019-12-18 13:56:54 +0300

    Auth fix

--- as-15-8/as-15-8-pmk/as-15-8-dispatcher/src/main/java/controllers/ResultsController.java
+++ as-15-8/as-15-8-pmk/as-15-8-dispatcher/src/main/java/controllers/ResultsController.java
@@ -134,7 +134,7 @@ public class ResultsController {
         return resultRepo.getCheckUnitJobResultsByArrangementId(arrangementId);
     }
 
-    @PreAuthorize("hasRole('ROLE_MANAGE_ARRANGEMENTS')")
+    @PreAuthorize("hasRole('ROLE_MANAGE_ARRANGEMENT')")
     @PutMapping(path = "/stop_arrangement")
     public boolean stopArrangement(@RequestParam("id") Long arrangementId){
         List<Result> results = resultRepo.findAllByArrangementId(arrangementId);
