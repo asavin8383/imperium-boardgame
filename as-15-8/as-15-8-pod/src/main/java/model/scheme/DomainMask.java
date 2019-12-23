f507bca1bcd56198e47f4c28f754c90c43945396
pvorobiev <pvorobiev@ec-leasing.ru>
2019-12-23 18:28:01 +0300

    lil

--- as-15-8/as-15-8-pod/src/main/java/model/scheme/DomainMask.java
+++ as-15-8/as-15-8-pod/src/main/java/model/scheme/DomainMask.java
@@ -1,6 +1,7 @@
 package model.scheme;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -14,6 +15,7 @@ import java.util.Set;
 @Entity
 @Table(schema = "sor", name = "domain_masks")
 @Data
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class DomainMask implements Serializable {
 
     @Id
