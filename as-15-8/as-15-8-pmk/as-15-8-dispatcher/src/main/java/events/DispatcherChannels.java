9e9686e89b92829ca12cdbeadb6ed21a47fe7afb
ashabalin <ashabalin@ec-leasing.ru>
2019-12-30 17:01:03 +0300

    kafka streams fix

--- as-15-8/as-15-8-pmk/as-15-8-dispatcher/src/main/java/events/DispatcherChannels.java
+++ as-15-8/as-15-8-pmk/as-15-8-dispatcher/src/main/java/events/DispatcherChannels.java
@@ -2,9 +2,6 @@ package events;
 
 import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.cloud.stream.annotation.Input;
-import org.springframework.cloud.stream.annotation.Output;
-import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.SubscribableChannel;
 
 /**
  * Creation date: 06.08.2019
