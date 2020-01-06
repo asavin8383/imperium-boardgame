3867fc274917fc3d6557c6781c145d3f4d0c822d
ashabalin <ashabalin@ec-leasing.ru>
2020-01-06 15:34:50 +0300

    dispatcher streams serdes

--- as-15-8/as-15-8-pmk/as-15-8-dispatcher/src/main/java/events/handlers/ResultsHandler.java
+++ as-15-8/as-15-8-pmk/as-15-8-dispatcher/src/main/java/events/handlers/ResultsHandler.java
@@ -3,8 +3,8 @@ package events.handlers;
 import analysis.CheckUnitResult;
 import checkUnits.CheckUnitKey;
 import events.DispatcherChannels;
+import events.serdes.SerdesFactory;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Materialized;
@@ -13,10 +13,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.Input;
 import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
-import org.springframework.kafka.support.serializer.JsonSerializer;
-import org.springframework.messaging.Message;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -32,22 +28,21 @@ public class ResultsHandler {
             @Input(DispatcherChannels.INPUT_ANALYSIS_RESULTS) KStream<CheckUnitKey, CheckUnitResult> analysisResultsStream,
             @Input(DispatcherChannels.INPUT_JOB_NOTIFICATIONS) KStream<CheckUnitKey, CheckUnitResult> notificationsStream
     ){
-        analysisResultsStream.peek((key, result) -> {
-            log.info("\n   ---->>> Принято сообщение с анализом результатов проверки: " +
+        analysisResultsStream.peek((key, result) ->
+                log.info("\n   ---->>> Принято сообщение с анализом результатов проверки: " +
                     "мероприятие: " + key.getArrangementId() + ", " +
-                    result.getJobID() + ", " + result.getCheckUnit().getValue() + ", результат: " + result.getCheckResult());
-        }).merge(notificationsStream
-            .peek((key, result) -> {
+                    result.getJobID() + ", " + result.getCheckUnit().getValue() + ", результат: " + result.getCheckResult()))
+        .merge(notificationsStream
+            .peek((key, result) ->
                     log.info("\n   ---->>> Принято сообщение с уведомлением от проверки: " +
-                            "мероприятие: " + key.getArrangementId() + ", " +
-                            result.getJobID() + ", " + result.getCheckUnit().getValue() + ", результат: " + result.getCheckResult());
-            })
+                        "мероприятие: " + key.getArrangementId() + ", " +
+                        result.getJobID() + ", " + result.getCheckUnit().getValue() + ", результат: " + result.getCheckResult()))
         ).groupByKey()
         .reduce((oldMessage, newMessage) -> newMessage,
                 Materialized.<CheckUnitKey, CheckUnitResult, KeyValueStore<Bytes, byte[]>>
                     as(resultsTableName)
-                    .withKeySerde(Serdes.serdeFrom(new JsonSerializer<>(), new JsonDeserializer<>()))
-                    .withValueSerde(Serdes.serdeFrom(new JsonSerializer<>(), new JsonDeserializer<>()))
+                    .withKeySerde(SerdesFactory.createSerde(CheckUnitKey.class))
+                    .withValueSerde(SerdesFactory.createSerde(CheckUnitResult.class))
         );
     }
 }
