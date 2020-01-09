b61b8d7506160e96c223b9c417df2b4914d89534
ashabalin <ashabalin@ec-leasing.ru>
2020-01-09 16:43:26 +0300

    pmk serdes fix

--- as-15-8/as-15-8-messages/src/main/java/analysis/CheckUnitResult.java
+++ as-15-8/as-15-8-messages/src/main/java/analysis/CheckUnitResult.java
@@ -1,6 +1,7 @@
 package analysis;
 
 import checkUnits.CheckUnit;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import enums.CheckUnitJobResult;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonTypeInfo(include= JsonTypeInfo.As.WRAPPER_OBJECT, use= JsonTypeInfo.Id.CLASS)
 public class CheckUnitResult {
 
     /** Идентифиатор задания */
--- as-15-8/as-15-8-pmk/as-15-8-dispatcher/src/main/java/events/handlers/ResultsHandler.java
+++ as-15-8/as-15-8-pmk/as-15-8-dispatcher/src/main/java/events/handlers/ResultsHandler.java
@@ -3,7 +3,9 @@ package events.handlers;
 import analysis.CheckUnitResult;
 import checkUnits.CheckUnitKey;
 import events.DispatcherChannels;
+import events.serdes.SerdesFactory;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.streams.kstream.KStream;
@@ -12,7 +14,6 @@ import org.apache.kafka.streams.state.KeyValueStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.kafka.support.serializer.JsonSerde;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -25,16 +26,19 @@ public class ResultsHandler {
 
     @StreamListener(DispatcherChannels.INPUT_RESULTS)
     public void processResults(KStream<CheckUnitKey, CheckUnitResult> resultsStream){
-        resultsStream/*.peek((key, result) ->
+        Serde<CheckUnitKey> keySerde = SerdesFactory.createSerde(CheckUnitKey.class);
+        Serde<CheckUnitResult> valueSerde = SerdesFactory.createSerde(CheckUnitResult.class);
+
+        resultsStream.peek((key, result) ->
                 log.info("\n   ---->>> Принято сообщение с анализом результатов проверки: " +
                     "мероприятие: " + key.getArrangementId() + ", " +
-                    result.getJobID() + ", " + result.getCheckUnit().getValue() + ", результат: " + result.getCheckResult()))*/
-        .groupByKey(Grouped.with(new JsonSerde<>(), new JsonSerde<>()))
+                    result.getJobID() + ", " + result.getCheckUnit().getValue() + ", результат: " + result.getCheckResult()))
+        .groupByKey(Grouped.with(keySerde, valueSerde))
         .reduce((oldMessage, newMessage) -> newMessage,
                 Materialized.<CheckUnitKey, CheckUnitResult, KeyValueStore<Bytes, byte[]>>
                     as(resultsTableName)
-                    .withKeySerde(new JsonSerde<>())
-                    .withValueSerde(new JsonSerde<>())
+                    .withKeySerde(keySerde)
+                    .withValueSerde(valueSerde)
         );
     }
 }
