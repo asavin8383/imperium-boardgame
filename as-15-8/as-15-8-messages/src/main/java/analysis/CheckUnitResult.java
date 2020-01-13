0d8d955c576f8c6d1d78c624c059d3e68c14dc9c
ashabalin <ashabalin@ec-leasing.ru>
2020-01-13 17:49:51 +0300

    dispatcher controllers

--- as-15-8/as-15-8-messages/src/main/java/analysis/CheckUnitResult.java
+++ as-15-8/as-15-8-messages/src/main/java/analysis/CheckUnitResult.java
@@ -2,16 +2,10 @@ package analysis;
 
 import checkUnits.CheckUnit;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.google.common.base.CaseFormat;
 import enums.CheckUnitJobResult;
 import lombok.*;
 
-import java.lang.reflect.Type;
-import java.time.LocalDateTime;
-import java.util.Comparator;
 import java.util.Date;
-
-import static com.sun.org.glassfish.external.statistics.impl.StatisticImpl.START_TIME;
 
 @Data
 @NoArgsConstructor
