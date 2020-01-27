f93f313364c77da78c6983e5e0ad7597bf894c67
danikanov <danikanov@ec-leasing.ru>
2020-01-27 12:34:32 +0300

    ppt фильтр по табам со статусами в списке меропритятий fix

--- as-15-8/as-15-8-ppt/src/main/java/repositories/impl/FormalTaskRepositoryAdvancedImpl.java
+++ as-15-8/as-15-8-ppt/src/main/java/repositories/impl/FormalTaskRepositoryAdvancedImpl.java
@@ -1,24 +1,23 @@
 package repositories.impl;
 
-import java.util.ArrayList;
-import java.util.List;
+import enums.ExecutionStatus;
+import model.task.FormalTask;
+import model.task.FormalTask_;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.query.QueryUtils;
+import org.springframework.stereotype.Repository;
+import repositories.FormalTaskRepositoryAdvanced;
+import repositories.helpers.CriteriaHelper;
 
 import javax.persistence.EntityManager;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.query.QueryUtils;
-import org.springframework.stereotype.Repository;
-
-import model.task.FormalTask;
-import model.task.FormalTask_;
-import repositories.FormalTaskRepositoryAdvanced;
-import repositories.helpers.CriteriaHelper;
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class FormalTaskRepositoryAdvancedImpl implements FormalTaskRepositoryAdvanced {
@@ -31,7 +30,7 @@ public class FormalTaskRepositoryAdvancedImpl implements FormalTaskRepositoryAdv
 	}
 
 	@Override
-	public Page<FormalTask> findPage(Long id, String operator, String fgisId, Pageable pageable) {
+	public Page<FormalTask> findPage(List<ExecutionStatus> statuses, Long id, String operator, String fgisId, Pageable pageable) {
 		CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
 		CriteriaQuery<FormalTask> select = criteriaBuilder.createQuery(FormalTask.class);
 		Root<FormalTask> fromFormalTask = select.from(FormalTask.class);
@@ -47,6 +46,11 @@ public class FormalTaskRepositoryAdvancedImpl implements FormalTaskRepositoryAdv
 	    if (fgisId != null) {
 	    	predicates.add(criteriaBuilder.like(criteriaBuilder.upper(fromFormalTask.get(FormalTask_.FGIS_ID)), "%" + fgisId.toUpperCase() + "%"));
 		}
+
+	    if (statuses != null &&  statuses.size()>0) {
+	    	predicates.add(fromFormalTask.get(FormalTask_.STATUS).in(statuses));
+		}
+
 	    select.where(predicates.toArray(new Predicate[0]));
 	    
 	    select.orderBy(QueryUtils.toOrders(pageable.getSort(), fromFormalTask, criteriaBuilder));
