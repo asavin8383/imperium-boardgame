5b60b30d28a3a721b7396b44c3609db6be3938ad
danikanov <danikanov@ec-leasing.ru>
2020-03-04 16:59:32 +0300

    messages execution status fix, postman PMK screenshot api fix

--- as-15-8/as-15-8-messages/src/main/java/enums/ExecutionStatus.java
+++ as-15-8/as-15-8-messages/src/main/java/enums/ExecutionStatus.java
@@ -5,18 +5,19 @@ import lombok.Getter;
 
 @AllArgsConstructor
 public enum ExecutionStatus {
+
 	RUNNING(0, "Выполняется"),
 	NEW(1, "Новое"),
 	FORMED(2, "Готово к включению в расписание"),
 	SCHEDULED(3, "Запланировано"),
 	STOPPING(4, "Останавливается"),
 	STOPPED(5, "Остановлено"),
-	FINISHED(6, "Выполнено"),
-	ACT_SENT(7, "Отправлен акт"),
-	CLOSED(8, "Закрыто"),
-	ERROR(9, "Ошибка"),
-	STOPPED_BY_SERVICE_MODE(10,"Остановлено при переходе в сервисный режим"),
-	STOPPED_BY_MAX_CHECK_UNITS(11, "Остановлено по превышению количества ИРТЗ");
+	STOPPED_BY_SERVICE_MODE(6,"Остановлено при переходе в сервисный режим"),
+	STOPPED_BY_MAX_CHECK_UNITS(7, "Остановлено по превышению количества ИРТЗ"),
+	FINISHED(8, "Выполнено"),
+	ACT_SENT(9, "Отправлен акт"),
+	CLOSED(10, "Закрыто"),
+	ERROR(11, "Ошибка");
 
 	@Getter
 	private int priority;
