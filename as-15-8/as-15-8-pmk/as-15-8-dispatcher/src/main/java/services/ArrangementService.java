442774829e1867e5d19b262f1ddbe1653352c4d9
danikanov <NXjdbgNGTM>
2020-11-10 10:37:53 +0300

    dispatcher test @Async functionality

--- as-15-8/as-15-8-pmk/as-15-8-dispatcher/src/main/java/services/ArrangementService.java
+++ as-15-8/as-15-8-pmk/as-15-8-dispatcher/src/main/java/services/ArrangementService.java
@@ -84,8 +84,8 @@ public class ArrangementService {
         stoppedArrangements.clear();
     }
 
-    @Scheduled(cron = "0 46 10 * * ?")
-    private void stopAllRunningArrangementsByDayGone() {
+    @Scheduled(cron = "0 57 10 * * ?")
+    void stopAllRunningArrangementsByDayGone() {
         try {
             log.info("Попытка завершения всех мероприятий по шедулеру на текущий день");
             stopAllRunningArrangements(Reason.MANUAL);
