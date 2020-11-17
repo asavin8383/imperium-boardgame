dfc54131a9f3c80211ff9cf437c07e41ae877ad7
danikanov <NXjdbgNGTM>
2020-11-17 09:46:37 +0300

    dispatcher cron test

--- as-15-8/as-15-8-pmk/as-15-8-dispatcher/src/main/java/services/ArrangementService.java
+++ as-15-8/as-15-8-pmk/as-15-8-dispatcher/src/main/java/services/ArrangementService.java
@@ -79,7 +79,7 @@ public class ArrangementService {
 
     @Async
     //@Scheduled(cron = "0 0 0 * * ?")
-    @Scheduled(cron = "0 44 09 * * ?")
+    @Scheduled(cron = "0 50 09 * * ?")
     void clearStoppedArrangements() {
         evictCaches();
         stoppedArrangements.clear();
