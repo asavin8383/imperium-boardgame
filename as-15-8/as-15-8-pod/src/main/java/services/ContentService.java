c678a4d72fb63579d79713540b9d76f40c35ceed
danikanov <NXjdbgNGTM>
2020-11-24 21:28:47 +0300

    pod debug log off

--- as-15-8/as-15-8-pod/src/main/java/services/ContentService.java
+++ as-15-8/as-15-8-pod/src/main/java/services/ContentService.java
@@ -140,7 +140,6 @@ public class ContentService {
              result.addAll(contentViewsBstch);
         });
 
-        logTime("Join c temp таблицей ", startTime);
         return result;
     }
 
