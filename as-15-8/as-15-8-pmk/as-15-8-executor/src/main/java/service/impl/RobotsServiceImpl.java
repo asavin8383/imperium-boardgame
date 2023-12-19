84254fdfc5d146f84e83868ccfc52d63bcb312c9
Ivan Filimonov <ifilimonov@ec-leasing.ru>
2023-12-19 13:32:46 +0300

    feat (executor): added logs

--- as-15-8/as-15-8-pmk/as-15-8-executor/src/main/java/service/impl/RobotsServiceImpl.java
+++ as-15-8/as-15-8-pmk/as-15-8-executor/src/main/java/service/impl/RobotsServiceImpl.java
@@ -27,167 +27,175 @@ import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Сервис управления роботами Selenium
- * @author shabalinAI
  *
+ * @author shabalinAI
  */
 @Service
 @Slf4j
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class RobotsServiceImpl implements CheckUnitVerificationService {
-	
-	/** Список роботов */
-	private final RobotsFactory robotsFactory;
 
-	private final ExecutorProperties executorProps;
-	
+    /**
+     * Список роботов
+     */
+    private final RobotsFactory robotsFactory;
+
+    private final ExecutorProperties executorProps;
+
     private volatile Set<Robot> robots = ConcurrentHashMap.newKeySet();
-    
-    private boolean isRunning = false;
-	long webdriverTimeout;
 
-	@PostConstruct
-	private void countTimeout() {
-		//Рассчитываем таймаут исходя из таймаута сервиса executor
-		long timeout = 30;
-		int retryAttempts = executorProps.getExecutor().getMaxRetryAttempts();
-		int retryDelay = executorProps.getExecutor().getMaxRetryDelay();
-		if (executorProps.getExecutor().getMaxRetryAttempts() > 0) {
-			long countTimeout = (long) ((Optional.ofNullable(executorProps.getExecutor().getTimeout())
-					.orElse(180) * 0.9 - (long) (retryAttempts - 1) * retryDelay) / retryAttempts);
-			if (countTimeout > timeout) {
-				timeout = countTimeout;
-			}
-		}
-		webdriverTimeout = timeout;
-		log.info("Рассчитанный таймаут одной проверки составит {} секунд", webdriverTimeout);
-	}
+    private boolean isRunning = false;
+    long webdriverTimeout;
+
+    @PostConstruct
+    private void countTimeout() {
+        //Рассчитываем таймаут исходя из таймаута сервиса executor
+        long timeout = 30;
+        int retryAttempts = executorProps.getExecutor().getMaxRetryAttempts();
+        int retryDelay = executorProps.getExecutor().getMaxRetryDelay();
+        if (executorProps.getExecutor().getMaxRetryAttempts() > 0) {
+            long countTimeout = (long) ((Optional.ofNullable(executorProps.getExecutor().getTimeout())
+                    .orElse(180) * 0.9 - (long) (retryAttempts - 1) * retryDelay) / retryAttempts);
+            if (countTimeout > timeout) {
+                timeout = countTimeout;
+            }
+        }
+        webdriverTimeout = timeout;
+        log.info("Рассчитанный таймаут одной проверки составит {} секунд", webdriverTimeout);
+    }
 
 
     public Map<AccessToolUnit, List<CheckUnitType>> getSupportedTypes() {
-    	return new HashMap<AccessToolUnit, List<CheckUnitType>>(){{
-    		put(AccessToolUnit.SEARCH_SYSTEM, Arrays.asList(CheckUnitType.URL, CheckUnitType.DOMAIN, CheckUnitType.SEARCH_PHRASE));
-			put(AccessToolUnit.PROXY, Arrays.asList(CheckUnitType.URL, CheckUnitType.DOMAIN));
-			put(AccessToolUnit.VPN, Arrays.asList(CheckUnitType.URL, CheckUnitType.DOMAIN));
-			put(AccessToolUnit.CAMELEO_XYZ, Collections.singletonList(CheckUnitType.URL));
-			put(AccessToolUnit.ANONYMIZER, Collections.singletonList(CheckUnitType.URL));
-			put(AccessToolUnit.HIDEMYASS, Collections.singletonList(CheckUnitType.URL));
-			put(AccessToolUnit.HOLA, Collections.singletonList(CheckUnitType.URL));
-			put(AccessToolUnit.EXTENSION, Collections.singletonList(CheckUnitType.URL));
-			put(AccessToolUnit.GOOGLE_API, Arrays.asList(CheckUnitType.URL, CheckUnitType.DOMAIN, CheckUnitType.SEARCH_PHRASE));
-		}};
+        return new HashMap<AccessToolUnit, List<CheckUnitType>>() {{
+            put(AccessToolUnit.SEARCH_SYSTEM, Arrays.asList(CheckUnitType.URL, CheckUnitType.DOMAIN, CheckUnitType.SEARCH_PHRASE));
+            put(AccessToolUnit.PROXY, Arrays.asList(CheckUnitType.URL, CheckUnitType.DOMAIN));
+            put(AccessToolUnit.VPN, Arrays.asList(CheckUnitType.URL, CheckUnitType.DOMAIN));
+            put(AccessToolUnit.CAMELEO_XYZ, Collections.singletonList(CheckUnitType.URL));
+            put(AccessToolUnit.ANONYMIZER, Collections.singletonList(CheckUnitType.URL));
+            put(AccessToolUnit.HIDEMYASS, Collections.singletonList(CheckUnitType.URL));
+            put(AccessToolUnit.HOLA, Collections.singletonList(CheckUnitType.URL));
+            put(AccessToolUnit.EXTENSION, Collections.singletonList(CheckUnitType.URL));
+            put(AccessToolUnit.GOOGLE_API, Arrays.asList(CheckUnitType.URL, CheckUnitType.DOMAIN, CheckUnitType.SEARCH_PHRASE));
+        }};
     }
 
-	@Override
-	public ExecutionJobResult run(Long jobId, CheckUnitJob checkUnitJob) throws ExecutionException {
-		try {
-			String robotName = "jobID = " + jobId +
+    @Override
+    public ExecutionJobResult run(Long jobId, CheckUnitJob checkUnitJob) throws ExecutionException {
+        try {
+            String robotName = "jobID = " + jobId +
                     " accessTool = " + checkUnitJob.getAccessTool() +
                     " checkUnit = " + checkUnitJob.getCheckUnit().getValue();
 			/*if(!this.isRunning)
 				throw new ExecutionException("Ошибка при запуске проверки запрещенного ресурса. Сервис проверки остановлен!");*/
-			log.info("Запуск робота: " + robotName);
+            log.info(String.format("Запуск робота: %s", robotName));
+            log.info(String.format("Параметры executor'a:\nmaxRetryAttempts=%s\nmaxRetryDelay=%s",
+                    executorProps.getExecutor().getMaxRetryAttempts(),
+                    executorProps.getExecutor().getMaxRetryDelay()));
 
-			Robot robot = robotsFactory.createRobot(checkUnitJob.getAccessTool());
-			robot.setRemainingAttempts(executorProps.getExecutor().getMaxRetryAttempts());
-			
-			robots.add(robot);
-			
-			ExecutionJobResult message;
-			boolean needToStop = true;
-			try{
-				message = runWithRetry(robot, checkUnitJob.getCheckUnit());
-			} catch (Exception ex) {
-				if(ex instanceof ExecutionException) {
-					if(ex instanceof Captcha_ExecutionException  || ex instanceof BadIP_ExecutionExeption) {
-						needToStop = false;
-					}
-					throw (ExecutionException)ex;
-				} else
-					throw new ExecutionException("Ошибка при выполнении скрипта робота", ex);
-			} finally {
-				if(needToStop && robot != null) {
-					try {
-						robot.destroy();
-					} catch (IOException ex) {
-						log.error("Ошибка при закрытии скрипта", ex);
-					}
-				}
-			}
-			robots.remove(robot);
-			message.setCheckUnit(checkUnitJob.getCheckUnit());
-	        message.setAccessTool(checkUnitJob.getAccessTool());
+            Robot robot = robotsFactory.createRobot(checkUnitJob.getAccessTool());
+            robot.setRemainingAttempts(executorProps.getExecutor().getMaxRetryAttempts());
 
-			log.info("Робот успешно завершил работу: "+robotName);
-			return message;
-		} catch(Exception ex) {
-            if(ex instanceof ExecutionException)
+            robots.add(robot);
+
+            ExecutionJobResult message;
+            boolean needToStop = true;
+            try {
+                message = runWithRetry(robot, checkUnitJob.getCheckUnit());
+            } catch (Exception ex) {
+                if (ex instanceof ExecutionException) {
+                    if (ex instanceof Captcha_ExecutionException || ex instanceof BadIP_ExecutionExeption) {
+                        needToStop = false;
+                    }
+                    throw (ExecutionException) ex;
+                } else
+                    throw new ExecutionException("Ошибка при выполнении скрипта робота", ex);
+            } finally {
+                if (needToStop && robot != null) {
+                    try {
+                        robot.destroy();
+                    } catch (IOException ex) {
+                        log.error("Ошибка при закрытии скрипта", ex);
+                    }
+                }
+            }
+            robots.remove(robot);
+            message.setCheckUnit(checkUnitJob.getCheckUnit());
+            message.setAccessTool(checkUnitJob.getAccessTool());
+
+            log.info("Робот успешно завершил работу: " + robotName);
+            return message;
+        } catch (Exception ex) {
+            if (ex instanceof ExecutionException)
                 throw ex;
             else
                 throw new ExecutionException("Ошибка при выполнении скрипта робота", ex);
-		}
-	}
+        }
+    }
 
-	public ExecutionJobResult runWithRetry(Robot robot, CheckUnit checkUnit) {
-		try {
-			robot.setRemainingAttempts(robot.getRemainingAttempts() - 1);
-			log.info("Запуск {}-й попытки проверки ресурса: {}", executorProps.getExecutor().getMaxRetryAttempts() - robot.getRemainingAttempts(), checkUnit.getValue());
-			boolean throwExceptionByCaptchaOrBadIP = true;
-			if (robot.getRemainingAttempts()==0) {
-				throwExceptionByCaptchaOrBadIP = false;
-			}
-			return robot.run(checkUnit, webdriverTimeout, throwExceptionByCaptchaOrBadIP);
-		} catch (Captcha_ExecutionException | BadIP_ExecutionExeption | WebDriverException |
-				 InternalError_ExecutionException ex) {
-			if (robot.getRemainingAttempts() > 0) {
-				log.warn("Будет выполнен {}-й перезапуск проверки ресурса {} по следующей причине: {}", executorProps.getExecutor().getMaxRetryAttempts() - robot.getRemainingAttempts(), checkUnit.getValue(), ex.getMessage());
-				try {
-					Thread.sleep(executorProps.getExecutor().getMaxRetryDelay() * 1000);
-				} catch (InterruptedException e) {
-					throw new RuntimeException(e);
-				}
-				try {
-					robot.destroy();
-				} catch (IOException e) {
-					log.error("Ошибка при закрытии скрипта", e);
-				}
-				return runWithRetry(robot, checkUnit);
-			} else {
-				throw ex;
-			}
+    public ExecutionJobResult runWithRetry(Robot robot, CheckUnit checkUnit) {
+        try {
+            robot.setRemainingAttempts(robot.getRemainingAttempts() - 1);
+            log.info("Запуск {}-й попытки проверки ресурса: {}", executorProps.getExecutor().getMaxRetryAttempts() - robot.getRemainingAttempts(), checkUnit.getValue());
+            log.info(String.format("Параметры executor'a:\nmaxRetryAttempts=%s\nmaxRetryDelay=%s",
+                    executorProps.getExecutor().getMaxRetryAttempts(),
+                    executorProps.getExecutor().getMaxRetryDelay()));
+            boolean throwExceptionByCaptchaOrBadIP = true;
+            if (robot.getRemainingAttempts() == 0) {
+                throwExceptionByCaptchaOrBadIP = false;
+            }
+            return robot.run(checkUnit, webdriverTimeout, throwExceptionByCaptchaOrBadIP);
+        } catch (Captcha_ExecutionException | BadIP_ExecutionExeption | WebDriverException |
+                 InternalError_ExecutionException ex) {
+            if (robot.getRemainingAttempts() > 0) {
+                log.warn("Будет выполнен {}-й перезапуск проверки ресурса {} по следующей причине: {}", executorProps.getExecutor().getMaxRetryAttempts() - robot.getRemainingAttempts(), checkUnit.getValue(), ex.getMessage());
+                try {
+                    Thread.sleep(executorProps.getExecutor().getMaxRetryDelay() * 1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                try {
+                    robot.destroy();
+                } catch (IOException e) {
+                    log.error("Ошибка при закрытии скрипта", e);
+                }
+                return runWithRetry(robot, checkUnit);
+            } else {
+                throw ex;
+            }
 
-		}
-	}
-	
-	@Override
-	public void start() {
-		this.isRunning = true;
-	}
+        }
+    }
 
-	@Override
-	public void stop() {
-		isRunning = false;
-		log.info("\n\n-----------------------------\n"
-				+ "Oстановка активных роботов..."
-				+ "\n-----------------------------\n");
-		for(Robot robot : robots) {
-			try {
-				robot.destroy();
-			} catch (IOException ex) {
-				log.error("Ошибка при остановке робота", ex);
-			}
-		}
-		log.info("\n\n-----------------------------\n"
-				+ "Pоботы успешно остановлены"
-				+ "\n-----------------------------\n");
-	}
+    @Override
+    public void start() {
+        this.isRunning = true;
+    }
 
-	@Override
-	public boolean isRunning() {
-		return this.isRunning;
-	}
-	
-	@Override
-	public int getPhase() {
-		return AbstractMessageListenerContainer.DEFAULT_PHASE - 10;
-	}
+    @Override
+    public void stop() {
+        isRunning = false;
+        log.info("\n\n-----------------------------\n"
+                + "Oстановка активных роботов..."
+                + "\n-----------------------------\n");
+        for (Robot robot : robots) {
+            try {
+                robot.destroy();
+            } catch (IOException ex) {
+                log.error("Ошибка при остановке робота", ex);
+            }
+        }
+        log.info("\n\n-----------------------------\n"
+                + "Pоботы успешно остановлены"
+                + "\n-----------------------------\n");
+    }
+
+    @Override
+    public boolean isRunning() {
+        return this.isRunning;
+    }
+
+    @Override
+    public int getPhase() {
+        return AbstractMessageListenerContainer.DEFAULT_PHASE - 10;
+    }
 }
