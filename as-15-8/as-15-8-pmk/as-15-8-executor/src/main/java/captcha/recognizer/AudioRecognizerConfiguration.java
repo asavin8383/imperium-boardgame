d880465ca1721f86ecba322211e06f6138ae4d64
ashabalin <ashabalin@ec-leasing.ru>
2023-12-21 10:52:40 +0300

    rm heepsize

--- as-15-8/as-15-8-pmk/as-15-8-executor/src/main/java/captcha/recognizer/AudioRecognizerConfiguration.java
+++ as-15-8/as-15-8-pmk/as-15-8-executor/src/main/java/captcha/recognizer/AudioRecognizerConfiguration.java
@@ -3,18 +3,22 @@ package captcha.recognizer;
 import org.springframework.aop.framework.ProxyFactoryBean;
 import org.springframework.aop.target.CommonsPool2TargetSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AudioRecognizerConfiguration {
 
+    @Value("$spring.cloud.stream.bindings.jobs.consumer.concurrency")
+    private int maxConcurrent;
+
     @Bean
     public CommonsPool2TargetSource AudioRecognizerPooledTargetSource() {
         final CommonsPool2TargetSource commonsPoolTargetSource = new CommonsPool2TargetSource();
         commonsPoolTargetSource.setTargetBeanName("audioRecognizer");
         commonsPoolTargetSource.setTargetClass(AudioRecognizer.class);
-        commonsPoolTargetSource.setMaxSize(6);
+        commonsPoolTargetSource.setMaxSize(this.maxConcurrent);
         return commonsPoolTargetSource;
     }
 
